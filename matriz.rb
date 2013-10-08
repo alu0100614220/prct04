2711adf3df47e8fc5606d1000dfef16dfe0f27df
CABELLO DELGADO <alu4049@cc112.(none)>
2013-10-08 17:23:50 +0100

    Version con fallos

--- matriz.rb
+++ matriz.rb
@@ -8,7 +8,34 @@
   STDOUT.flush
   n = gets.chomp
   puts "Matrices de " + n + "x" + n
-  a = [[[0]*n.to_i]*n.to_i]
-  b = [[[0]*n.to_i]*n.to_i]
-  c = [[[0]*n.to_i]*n.to_i]
+  a = [[0,0,0], [0,0,0], [0,0,0]]
+  b = [[0,0,0]*(n.to_i)]
+  c = [0]*n.to_i
+  d = [c]*n.to_i
   
+  for l in (0...n.to_i) do
+    for k in (0...n.to_i) do
+      puts d[l][k]
+    end
+  end
+  
+  for i in (0...(n.to_i)) do
+    for j in (0...(n.to_i)) do
+      puts "Introduza el valor del elemento [#{i}][#{j}]"
+      STDOUT.flush
+      aux = gets.chomp
+      d[i][j] = aux.to_i
+    end	
+  end
+  
+  #for l in 0..(n.to_i-1) do
+   # for k in 0..(n.to_i-1) do
+    #  puts c[l,k]
+    #end
+  #end
+  
+for l in (0...n.to_i) do
+    for k in (0...n.to_i) do
+      puts d[l][k]
+    end
+  end
