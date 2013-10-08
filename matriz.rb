82a8847d5afa33e653a17a67263e143dc1a544ef
CABELLO DELGADO <alu4049@cc112.(none)>
2013-10-08 18:03:37 +0100

    Version con multiplicacion

--- matriz.rb
+++ matriz.rb
@@ -9,24 +9,39 @@
   b = [[0,0,0], [0,0,0], [0,0,0]]
   c = [[0,0,0], [0,0,0], [0,0,0]]
   
-  for l in (0...3) do
-    for k in (0...3) do
-      puts a[l][k]
-    end
-  end
   
   for i in (0...3) do
     for j in (0...3) do
-      puts "Introduza el valor del elemento [#{i}][#{j}]"
+      puts "Introduza el valor del elemento [#{i}][#{j}] de la matriz 1"
       STDOUT.flush
       aux = gets.chomp
       a[i][j] = aux.to_i
     end	
   end
 
+ for i in (0...3) do
+    for j in (0...3) do
+      puts "Introduza el valor del elemento [#{i}][#{j}] de la matriz 2"
+      STDOUT.flush
+      aux = gets.chomp
+      b[i][j] = aux.to_i
+    end	
+ end
   
-for l in (0...3) do
+  puts "Multiplicacion de matrices: "
+  
+  for i in (0...3) do
+    for j in (0...3) do
+      sum = 0;
+      for k in (0...3) do
+	sum += (a[i][k] * b[k][j])
+      end
+      c[i][j] = sum
+    end	
+ end
+ 
+   for l in (0...3) do
     for k in (0...3) do
-      puts a[l][k]
+      puts c[l][k]
     end
   end
